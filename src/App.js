29f353405cfb2bef4f2dbd47b6f291f3233fadc5
Ramunas <zamrif@gmail.com>
2021-04-06 13:12:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { fetchBoardCanva } from "./apis/get";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { homePage } from "./views";
+import { BoardProvider } from "./context";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetchBoardCanva().then((result) => setData(result));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BoardProvider boardData={data}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={homePage} />
+        </Switch>
+      </BrowserRouter>
+    </BoardProvider>
   );
-}
+};
 
 export default App;
