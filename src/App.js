eb409aad327c9c265a3c7752c4e9c968b6cf4509
Ramunas <zamrif@gmail.com>
2021-04-06 17:58:57 +0300

    use memo to fetch new version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { fetchBoardCanva, addToBoard } from "./apis";
+import React, { useEffect, useState, useMemo } from "react";
+import { fetchBoardCanva, addToBoard, fetchBoardStatus } from "./apis";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { homePage } from "./views";
 import { BoardProvider } from "./context";
 
 const App = () => {
   const [data, setData] = useState();
+  const [version, setVersion] = useState(0);
 
-  useEffect(() => {
-    fetchBoardCanva().then((result) => setData(result));
-  }, []);
+  // useEffect(() => {
+  //   fetchBoardCanva().then((result) => setData(result));
+  // }, []);
+  // console.log(data);
+
+  useMemo(() => {
+    fetchBoardStatus().then((newVersion) => {
+      if (version !== newVersion) {
+        console.log("new version is avalible");
+        fetchBoardCanva().then((result) => setData(result));
+      }
+      setVersion(newVersion);
+    });
+  }, [version]);
+
+  console.log(version);
 
   // addToBoard({ userName: "rami", userColor: "orange", x: 1017, y: 1005 });
 
