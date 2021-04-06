ec586cceb9305da410f864eb027827ff2b15eed3
antanas andrulis <a.andrulis@yahoo.com>
2021-04-06 22:54:51 +0300

    color change on click implemented

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React, { useEffect, useState, useMemo } from "react";
-import { fetchBoardCanva, addToBoard, fetchBoardStatus } from "./apis";
+import React, { useEffect, useState } from "react";
+import { fetchBoardCanva } from "./apis";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { homePage } from "./views";
 import { BoardProvider } from "./context";
 
 const App = () => {
   const [data, setData] = useState();
-  // const build = useMemo(() => ({ version: 0 }), []);
 
   useEffect(() => {
     fetchBoardCanva().then((result) => setData(result));
   }, []);
-  // console.log(data);
-
-  // const BuildVersion = () => {
-  //   fetchBoardStatus().then((newVersion) => {
-  //     if (build.version < newVersion) {
-  //       console.log("new version is avalible");
-  //       // build.version = newVersion;
-  //       fetchBoardCanva().then((result) => setData(result));
-  //     }
-  //   });
-  // };
-  // BuildVersion();
-
-  // addToBoard({ userName: "rami", userColor: "orange", x: 1017, y: 1005 });
 
   return (
     <BoardProvider boardData={data}>
