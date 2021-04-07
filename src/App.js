90881a70f052639824f21f0240af8b8d5a1e9bbd
Ramunas <zamrif@gmail.com>
2021-04-07 09:52:43 +0300

    timout for build  func added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React, { useEffect, useState, useMemo } from "react";
-import { fetchBoardCanva, addToBoard, fetchBoardStatus } from "./apis";
+import { fetchBoardCanva, fetchBoardStatus } from "./apis";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { homePage } from "./views";
+import { HomePage } from "./views";
 import { BoardProvider } from "./context";
 
 const App = () => {
   const [data, setData] = useState();
-  // const build = useMemo(() => ({ version: 0 }), []);
+  const build = useMemo(() => ({ version: 0 }), []);
+
+  const BuildVersion = () => {
+    setInterval(() => {
+      fetchBoardStatus().then((newVersion) => {
+        if (build.version < newVersion) {
+          console.log("new build version is avalible");
+          build.version = newVersion;
+          fetchBoardCanva().then((result) => setData(result));
+        }
+      });
+    }, 1000);
+  };
 
   useEffect(() => {
     fetchBoardCanva().then((result) => setData(result));
+    BuildVersion();
   }, []);
-  // console.log(data);
-
-  // const BuildVersion = () => {
-  //   fetchBoardStatus().then((newVersion) => {
-  //     if (build.version < newVersion) {
-  //       console.log("new version is avalible");
-  //       // build.version = newVersion;
-  //       fetchBoardCanva().then((result) => setData(result));
-  //     }
-  //   });
-  // };
-  // BuildVersion();
-
-  // addToBoard({ userName: "rami", userColor: "orange", x: 1017, y: 1005 });
 
   return (
     <BoardProvider boardData={data}>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={homePage} />
+          <Route exact path="/" component={HomePage} />
         </Switch>
       </BrowserRouter>
     </BoardProvider>
