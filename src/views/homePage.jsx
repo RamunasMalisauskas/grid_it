b2742253b911a6f191f047a6def11df0b0a3e24f
Ramunas <zamrif@gmail.com>
2021-04-10 09:59:13 +0300

    random color/ data added to post

--- src/views/homePage.jsx
+++ src/views/homePage.jsx
@@ -5,20 +5,29 @@ import { addToBoard } from "../apis";
 export const HomePage = () => {
   const [x, setX] = useState();
   const [y, setY] = useState();
+  const [data, setData] = useState();
+  const randomColor = require("randomcolor");
+
   return (
     <>
       <Grid />
 
       <input type="number" value={x} onChange={(e) => setX(e.target.value)} />
       <input type="number" value={y} onChange={(e) => setY(e.target.value)} />
+      <input
+        type="text"
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+      />
 
       <button
         onClick={() => {
           addToBoard({
             userName: "rami",
-            userColor: "green",
+            userColor: randomColor(),
             x: parseInt(x),
             y: parseInt(y),
+            data: { text: data },
           });
         }}
       >
