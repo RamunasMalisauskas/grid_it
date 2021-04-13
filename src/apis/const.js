d0b792719db65fa0f49a8ddf13e23d5a8db3decf
Ramunas <zamrif@gmail.com>
2021-04-13 21:17:28 +0300

    delete api create/ added to Cell

--- src/apis/const.js
+++ src/apis/const.js
@@ -3,7 +3,8 @@ export const apiUrl = "https://simutis.dev/api";
 export const urlDir = {
   Board: "/board",
   BoardStatus: "/board/status",
-  CellStatus: "board/cell",
+  CellStatus: "/board/cell",
+  Delete: "/board/cell/delete",
 };
 
 export const postHeader = { "Content-Type": "application/json" };
--- src/apis/index.js
+++ src/apis/index.js
@@ -1,3 +1,4 @@
 export * from "./const";
 export * from "./get";
 export * from "./post";
+export * from "./delete";
