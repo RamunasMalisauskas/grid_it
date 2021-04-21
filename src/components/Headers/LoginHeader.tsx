201f3a2bf65e01ca417acec5fb56e0696f926bbc
Ramunas <zamrif@gmail.com>
2021-04-21 12:04:33 +0300

    auth from firebase used in form submit function

--- src/components/Headers/LoginHeader.tsx
+++ src/components/Headers/LoginHeader.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { setLogin, setUserName } from "../../state/actions";
 import { PrimaryButton, SupportButton, Input, Paragraph } from "../../components";
 import { CellDataType } from '../../apis/get'
+import { auth } from '../../firebase'
 
 type BoardDataState = {
     appData: {
@@ -17,7 +18,6 @@ type BoardDataState = {
 
 type NavPropsType = {
     loggedIn: boolean
-
 }
 
 
@@ -26,36 +26,94 @@ export const LoginHeader = () => {
     const loginStatus = useSelector((state: BoardDataState) => state.appData.login)
     const userName = useSelector((state: BoardDataState) => state.appData.name)
 
-    const handleLogin = (name: string, login: string): void => {
-        dispatch(setUserName(name));
-        dispatch(setLogin(login))
-        localStorage.setItem("userName", name);
-        localStorage.setItem("login", login);
-    };
+    const handleReg = useCallback<React.FormEventHandler<HTMLFormElement>>((e) => {
+        e.preventDefault()
+        const { target: {
+            "user name": { value: userName },
+            email: { value: email },
+            password: { value: pass },
+            "repPassword": { value: repPass }
+        } } = e
+        if (pass !== repPass) {
+            return
+        } else {
+            auth.createUserWithEmailAndPassword(email, pass)
+                .then(() => {
+                    dispatch(setLogin("loggedIn"));
+                    dispatch(setUserName(userName))
+                })
+                .catch(e => console.log(e))
+        }
+    }, [dispatch]);
+
+    const handleLogin = useCallback<React.FormEventHandler<HTMLFormElement>>((e) => {
+        e.preventDefault()
+        const { target: {
+            email: { value: email },
+            password: { value: pass }
+        } } = e
+        auth.signInWithEmailAndPassword(email, pass)
+            .then(() => dispatch(setLogin("loggedIn")))
+            .catch(e => console.log(e))
+    }, [dispatch]);
+
+    const handleLogout = useCallback(() => { console.log("logout"); dispatch(setLogin("loggedOut")) }, [dispatch])
+
+    const handleRegState = useCallback(() => { console.log("register"); dispatch(setLogin("Register")) }, [dispatch])
 
     return (
         <Nav loggedIn={loginStatus === "loggedIn"}>
-            {loginStatus === "loggedOut" &&
-                <form onSubmit={(e: React.SyntheticEvent): void => {
-                    e.preventDefault()
-                    handleLogin(userName, "loggedIn")
-                }}>
+            {loginStatus === "Register" &&
+                <form onSubmit={handleReg}>
                     <Input
                         type="text"
-                        name="enter your user name"
-                        value={userName}
-                        onChange={(e) => dispatch(setUserName(e.target.value))}
+                        name="user name"
+                        label="enter user name"
                     />
-
-
-                    <SupportButton type="submit"  >
-                        login
+                    <Input
+                        type="email"
+                        name="email"
+                        label="enter your email"
+                    />
+                    <Input
+                        type="password"
+                        name="password"
+                        label="enter password"
+                    />
+                    <Input
+                        type="password"
+                        name="repPassword"
+                        label="repeat password"
+                    />
+                    <SupportButton type="submit">
+                        Register
                     </SupportButton>
+                </form>
+            }
+            {loginStatus === "loggedOut" &&
+                <form onSubmit={handleLogin}>
+                    <Input
+                        type="email"
+                        name="email"
+                        label="enter your email"
+                    />
+                    <Input
+                        type="password"
+                        name="password"
+                        label="enter password"
+                    />
+                    <SupportButton type="submit">
+                        Login
+                    </SupportButton>
+
+                    <PrimaryButton onClick={handleRegState}>
+                        Register
+                    </PrimaryButton>
                 </form>
             }
 
             {loginStatus === "loggedIn" && <>
-                <PrimaryButton onClick={() => handleLogin("", "loggedOut")}>
+                <PrimaryButton onClick={handleLogout}>
                     logout
                 </PrimaryButton>
 
