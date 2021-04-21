a8ec7bb0fd9670465796180667d00738a978293c
Ramunas <zamrif@gmail.com>
2021-04-21 12:03:13 +0300

    firebase config added /exported

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth"
+
+let firebaseConfig = {
+    apiKey: "AIzaSyCMrr0HrIpG8ev_KVVv3w9n5rGicNqSMZU",
+    authDomain: "grid-it-app.firebaseapp.com",
+    projectId: "grid-it-app",
+    storageBucket: "grid-it-app.appspot.com",
+    messagingSenderId: "531223948375",
+    appId: "1:531223948375:web:ef9a1f1c9c28348c06c712"
+};
+
+const Firebase = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth()
+
+export default Firebase
