7c5d909f813a436ed86e4e57714fe151ad6246a4
Ramunas <zamrif@gmail.com>
2021-04-23 14:15:08 +0300

    firestore config added /user db /timestamp

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,24 @@
 import firebase from "firebase/app";
-import "firebase/auth"
+import "firebase/firestore";
+import "firebase/auth";
 
 let firebaseConfig = {
-    apiKey: "AIzaSyCMrr0HrIpG8ev_KVVv3w9n5rGicNqSMZU",
-    authDomain: "grid-it-app.firebaseapp.com",
-    projectId: "grid-it-app",
-    storageBucket: "grid-it-app.appspot.com",
-    messagingSenderId: "531223948375",
-    appId: "1:531223948375:web:ef9a1f1c9c28348c06c712"
+  apiKey: "AIzaSyCMrr0HrIpG8ev_KVVv3w9n5rGicNqSMZU",
+  authDomain: "grid-it-app.firebaseapp.com",
+  projectId: "grid-it-app",
+  storageBucket: "grid-it-app.appspot.com",
+  messagingSenderId: "531223948375",
+  appId: "1:531223948375:web:ef9a1f1c9c28348c06c712",
 };
 
 const Firebase = firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth()
+export const auth = firebase.auth();
 
-export default Firebase
+export const db = firebase.firestore();
+
+export const usersDB = db.collection("users");
+
+export const timeStamp = firebase.firestore.FieldValue.serverTimestamp()
+
+export default Firebase;
