ff74108e9d2306492713ff974d94d26acdbbb95e
Ramunas <zamrif@gmail.com>
2021-04-23 16:17:20 +0300

    redux dev tools added

--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import { combinedReducer } from "./reducers";
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const initialState = {}
 
-export const store = createStore(combinedReducer, initialState);
+export const store = createStore(combinedReducer, initialState, devToolsEnhancer({}));
