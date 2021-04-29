aa239c5b74cdfe38e2af85c958adf913fed34122
Ramunas <zamrif@gmail.com>
2021-04-29 09:52:28 +0300

    default theme created / provided

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from "./App";
 import "normalize.css";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/styles/defaultTheme.ts
@@ -0,0 +1,52 @@
+export const defaultTheme = {
+  colors: {
+    primary: "#d15585",
+    secondary: "#707070",
+    ligth: "#ffffff",
+    dark: "#2c2c2c",
+    support: "rgba(209, 209, 209, 0.4)",
+  },
+
+  size: {
+    xs: "0.3em",
+    s: "0.6em",
+    m: "1.0em",
+    l: "1.8em",
+    xl: "4em",
+    xxl: "10em",
+    xxxl: "18em",
+  },
+
+  fonts: {
+    family: {
+      primary: "'Zen Dots', cursive",
+      secondary: "'Montserrat', sans-serif",
+      support: "'Reenie Beanie', cursive",
+    },
+    weigth: {
+      regular: "300",
+      bold: "700",
+    },
+    size: {
+      regular: "24px",
+      large: "40px",
+    },
+  },
+
+  border: {
+    primary: "2px solid #d15585",
+    secondary: "2px solid #707070",
+    radius: {
+      primary: "0.2em",
+      secondary: "0.8em",
+    },
+  },
+
+  shadow: "1px 2px 3px 1px rgba(0, 0, 0, 0.4)",
+
+  transition: {
+    allFast: "all ease-in-out 0.1s",
+    allSlow: "all ease-in-out 0.3s",
+    allDelay: "all ease-in-out 0.3s 0.3s",
+  },
+};
