e75455d17a200d64e07c4f3825ce433991685e79
Ramunas <zamrif@gmail.com>
2021-04-29 11:44:19 +0300

    login status func. moved to initial state

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,24 +1,17 @@
-import React, { useEffect } from "react";
-import { LoginMenu, SideBarMenu, Grid, ClassMenu } from "../components";
-import { useDispatch } from "react-redux";
-import { setLogin } from "../state/actions";
-import { storageItems, log } from "../types/types";
+import React from "react";
+import {
+  LoginMenu,
+  SideBarMenu,
+  Grid,
+  ClassMenu,
+  WelcomeModal,
+} from "../components";
 
 export const HomePage: React.FC = () => {
-  const dispatch = useDispatch();
-  const localStatus = sessionStorage.getItem(storageItems.status);
-
-  const userLoginStatus = () => {
-    if (!localStatus) return;
-    if (localStatus === log.in) {
-      dispatch(setLogin(localStatus));
-    }
-  };
-
-  useEffect(() => userLoginStatus());
-
   return (
     <>
+      <WelcomeModal />
+
       <LoginMenu />
 
       <ClassMenu />
